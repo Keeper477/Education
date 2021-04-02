6a7d6be20b81b871a2eafd19eeb913f6ce6925c4
Keeper477 <serg.prok477@gmail.com>
2021-04-02 17:23:37 +0300

    Added prac_13

---
+++ Java/sem_4/prac_13/src/main/java/config/Student.java
@@ -0,0 +1,22 @@
+package config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+
+@Component
+public class Student {
+    @Value("${student.name}")
+    String name;
+    @Value("${student.last_name}")
+    String last_name;
+    @Value("${student.group}")
+    String group;
+    @PostConstruct
+    public void init(){
+        System.out.println(name + " " + last_name + " " + group);
+    }
+}
---
+++ Java/sem_4/prac_13/src/main/java/config/Test_13.java
@@ -0,0 +1,13 @@
+package config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test_13 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test_13.class, args);
+	}
+}
+
