5f3a173e0fc985a66d91a086c497a5d17e2dc235
Keeper477 <serg.prok477@gmail.com>
2021-04-02 13:31:06 +0300

    Change prac_10

--- Java/sem_4/prac_11/src/main/java/pack/DemoApplication.java
+++ Java/sem_4/prac_11/src/main/java/pack/DemoApplication.java
@@ -1,11 +1,15 @@
+package pack;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		Test_11 test11 = new Test_11();
+		test11.test();
 	}
-
 }
 
